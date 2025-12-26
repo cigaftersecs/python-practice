a53b5571c9b78f4e4a7b9132f5a85514c56500c9
Anas Malik <awananas300@gmail.com>
2025-12-26 10:41:12 +0500

    Add files via upload

---
+++ SecondProgram.py
@@ -0,0 +1,22 @@
+name = "Anas"
+age = 24
+price = 12.99
+
+print(name)
+print(age)
+print(price)
+print("my age is : ", age)
+print("may name is : ", name)
+
+age2 = age
+print(age2)
+
+print(type(name))
+print(type(age))
+print(type(price))
+
+cars = False
+count = None
+
+print(type(cars))
+print(type(count))
